ab653b4a7c140b574ee2bbe796f418d1e8bbfe2a
Pedro Costa <pedrocosta2802@gmail.com>
2025-08-16 13:36:33 -0300

    Update clock width

--- src/components/ClockPill.jsx
+++ src/components/ClockPill.jsx
@@ -22,7 +22,7 @@ export default function ClockPill() {
     return (
         <motion.div
             initial={{ width: 40 }}
-            animate={{ width: isOpen ? 170 : 40 }}
+            animate={{ width: isOpen ? 210 : 40 }}
             transition={{ type: "spring", stiffness: 300, damping: 25}}
             className="fixed top-4 left-4 flex items-center rounded-full bg-clockPillLight text-marrom 
                         dark:bg-marrom dark:text-creme shadow-md overflow-hidden cursor-pointer select-none"
